baac4aaca5c4d57eaf77b2ab626f75d7ee15d181
binay <binayakumar554@gmail.com>
2020-10-16 18:24:26 +0530

    my first commit Front page and side bar
    9-reality

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './Sidebar/sidebar/sidebar.component';
+import { FrontpageComponent } from './frontpage/frontpage/frontpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    FrontpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
