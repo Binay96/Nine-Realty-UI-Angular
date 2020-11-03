b103061e7e795006163b5b02ba5114bdee3756b5
Binay96 <binayakumar554@gmail.com>
2020-11-03 19:05:00 +0530

    residential-lease created

---
+++ src/app/Residental-Lease/residental-lease/residental-lease.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-residental-lease',
+  templateUrl: './residental-lease.component.html',
+  styleUrls: ['./residental-lease.component.css']
+})
+export class ResidentalLeaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
