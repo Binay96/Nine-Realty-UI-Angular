7b92d41e95c7403ac05fdabfde4033fe0420d3e4
Binay96 <binayakumar554@gmail.com>
2020-11-03 19:15:23 +0530

    All-images-and-file created

---
+++ src/app/rerano/rerano.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rerano',
+  templateUrl: './rerano.component.html',
+  styleUrls: ['./rerano.component.css']
+})
+export class ReranoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
